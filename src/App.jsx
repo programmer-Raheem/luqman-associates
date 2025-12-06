f74ac2345ad397b1857195dd5e51d7299fd15cba
Abdul Raheem <programmer-Raheem@users.noreply.github.com>
2025-12-06 21:09:53 +0500

    Fix Projects component import to match filename case

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import Projects from "./components/projects";
+import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import MovingHeadline from "./components/MovingHeadline";
 import About from "./components/About";
